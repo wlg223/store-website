3f43f7c9e7d787d731d222ff9c42c4c8b288e9f0
Wes Guarneri <weslee_guarneri@wess-mbp.lan>
2021-07-26 00:11:06 -0400

    commit 4

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,7 @@
 $(function() {
     var template = $('#hidden-template').html();
     
-    var results =  [{boxName:"Devito1",boxText:"Lorem Ipsum",boxImage:"https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Danny_DeVito_by_Gage_Skidmore.jpg/170px-Danny_DeVito_by_Gage_Skidmore.jpg"},{boxName:"devito2",boxText:"dolor sit amet",boxImage:"https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Danny_DeVito_by_Gage_Skidmore.jpg/170px-Danny_DeVito_by_Gage_Skidmore.jpg"},{boxName:"devito3",boxText:"consectetur",boxImage:"https://scontent.fwbw1-1.fna.fbcdn.net/v/t1.18169-9/558567_229706677127535_1123159924_n.jpg?_nc_cat=100&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=R98bqxTBnzcAX_1yUYr&_nc_ht=scontent.fwbw1-1.fna&oh=5a26a57326f92253681f3c13a0821501&oe=60F58CCD"}];
+    var results =  [{name:"Devito1",price:"Lorem Ipsum",image:"https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Danny_DeVito_by_Gage_Skidmore.jpg/170px-Danny_DeVito_by_Gage_Skidmore.jpg"},{name:"devito2",price:"dolor sit amet",image:"https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Danny_DeVito_by_Gage_Skidmore.jpg/170px-Danny_DeVito_by_Gage_Skidmore.jpg"},{name:"devito3",price:"consectetur",image:"https://scontent.fwbw1-1.fna.fbcdn.net/v/t1.18169-9/558567_229706677127535_1123159924_n.jpg?_nc_cat=100&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=R98bqxTBnzcAX_1yUYr&_nc_ht=scontent.fwbw1-1.fna&oh=5a26a57326f92253681f3c13a0821501&oe=60F58CCD"}];
   
   
   //reset 
@@ -13,9 +13,9 @@ $(function() {
       
       var myTemplate = $(template).clone();
      
-      $(myTemplate).find('#box-name').text(box.boxName);
-      $(myTemplate).find('#box-text').text(box.boxText);
-      $(myTemplate).find('#box-image').attr("src",box.boxImage);
+      $(myTemplate).find('#name').text(box.name);
+      $(myTemplate).find('#price').text(box.price);
+      $(myTemplate).find('#image').attr("src",box.image);
       
       $('#target').append(myTemplate);
     });
